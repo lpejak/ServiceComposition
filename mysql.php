45dbfe74e03422dead2dfb76dced08be88c54cef
lpejak <lpejak@riteh.hr>
2016-11-10 18:28:25 +0000

    mysql, nusoap, readme

---
+++ mysql.php
@@ -0,0 +1,44 @@
+<?php
+require_once "nusoap.php";
+
+
+function checkService($para){
+	zapisArray($para);
+	return 1;
+}
+
+function zapisArray($para){
+
+	$conn = new mysqli("localhost", "root", "grad", "baza");
+	foreach(array_slice($para, 2) as $p){
+		$sql = "INSERT INTO test (mean, stdev, broj) VALUES (". $para[0] .", " . $para[1] . ", " . $p  . ")";
+		$conn->query($sql);
+	}
+	$conn->close();
+}
+
+
+$server = new soap_server();
+$server->configureWSDL("status", "urn:status");
+
+$server->wsdl->addComplexType(
+	"randArray",
+        "complexType",
+        "array",
+        "",
+        "SOAP-ENC:Array",
+        array(),
+	array(array("ref"=>"SOAP-ENC:arrayType","wsdl:arrayType"=>"xsd:string[]")),
+        "xsd:string");
+
+$server->register("checkService",
+    array("para" => "tns:randArray"),
+    array("return" => "xsd:string"),
+    "urn:status",
+    "urn:status#checkService",
+    "rpc",
+    "encoded",
+    "Check if service is working by passing your name");
+
+$server->service($HTTP_RAW_POST_DATA);
+
