834d46a0c2ff913046aa85c28ac2b1a7953f509c
lpejak <lpejak@riteh.hr>
2016-11-29 18:24:14 +0100

    generator i monitoring update

--- python/ceilo_monitoring.py
+++ python/ceilo_monitoring.py
@@ -11,92 +11,110 @@ credentials = get_credentials()
 #Stvaramo ceilometerclient objekt pomocu get_client metode i pokazivaca na credentials objekt koji popunjava potrebne postavke za get_client metodu
 cclient = ceilometerclient.client.get_client(2, **credentials)
 
+#stvaramo datoteku za zapis metrika
+file = open('metrics.txt', 'a')
+
+#uzimamo vrijednosti id pojedinih instanci
 id1 = '3c420b81-2225-4243-97d4-c27032d4cefb'
 id2 = '59456355-77e0-4a4b-9d29-60a92c9cd396'
 id3 = '87f6a3d3-f635-4876-bdf3-c39c447466eb'
-tsStart = '2016-11-16T12:00:00Z'
-tsEnd = '2016-11-16T16:30:00Z'
 
-query1 = [dict(field='resource_id', op='eq', value=id1), dict(field='timestamp', op='gt', value=tsStart), dict(field='timestamp', op='lt', value=tsEnd)]
+#podesavamo vrijednosti pocetnog i krajnjeg datuma
+hourStart = 13
+hourEnd = 16
+minuteStart = 0
+minuteEnd = 0
+year = 2016
+month = 11
+day = 28
+#tsStart = '2016-11-28T13:10:00+01:00'
+#tsEnd = '2016-11-28T13:20:00+01:00'
 
-query2 = [dict(field='resource_id', op='eq', value=id2), dict(field='timestamp', op='gt', value=tsStart), dict(field='timestamp', op='lt', value=tsEnd)]
+while (hourStart != hourEnd or minuteStart != minuteEnd):
 
-query3 = [dict(field='resource_id', op='eq', value=id3), dict(field='timestamp', op='gt', value=tsStart), dict(field='timestamp', op='lt', value=tsEnd)]
+	if minuteStart == 0:
+		tsStart = str(year) + '-' + str(month) + '-' + str(day) +'T' + str(hourStart) + ':' + str(minuteStart) + str(minuteStart) + ':00+01:00'
+	else:
+		tsStart = str(year) + '-' + str(month) + '-' + str(day) +'T' + str(hourStart) + ':' + str(minuteStart) + ':00+01:00'
 
-while True:
-	for sample in cclient.statistics.list(meter_name = 'memory', q = query1):
-		memoryMax1=sample.max
-	for sample in cclient.statistics.list(meter_name = 'memory', q = query2):
-		memoryMax2=sample.max
-	for sample in cclient.statistics.list(meter_name = 'memory', q = query3):
-                memoryMax3=sample.max
+	if minuteStart == 50:
+		minuteStart = 0
+		hourStart += 1
+	else:
+		minuteStart += 10
+
+	if minuteStart == 0:
+		tsEnd = str(year) + '-' + str(month) + '-' + str(day) +'T' + str(hourStart) + ':' + str(minuteStart) + str(minuteStart) + ':00+01:00'
+	else:
+		tsEnd = str(year) + '-' + str(month) + '-' + str(day) +'T' + str(hourStart) + ':' + str(minuteStart) + ':00+01:00'
+
+	query1 = [dict(field='resource_id', op='eq', value=id1), dict(field='timestamp', op='ge', value=tsStart), dict(field='timestamp', op='le', value=tsEnd)]
+
+	query2 = [dict(field='resource_id', op='eq', value=id2), dict(field='timestamp', op='ge', value=tsStart), dict(field='timestamp', op='le', value=tsEnd)]
+
+	query3 = [dict(field='resource_id', op='eq', value=id3), dict(field='timestamp', op='ge', value=tsStart), dict(field='timestamp', op='le', value=tsEnd)]
+
+#	for sample in cclient.statistics.list(meter_name = 'memory', q = query1):
+#		memoryMax1=sample.max
+#	for sample in cclient.statistics.list(meter_name = 'memory', q = query2):
+#		memoryMax2=sample.max
+#	for sample in cclient.statistics.list(meter_name = 'memory', q = query3):
+#		memoryMax3=sample.max
+
 	for sample in cclient.statistics.list(meter_name = 'memory.usage', q = query1):
-		memoryUsageAvg1=sample.avg
-        for sample in cclient.statistics.list(meter_name = 'memory.usage', q = query2):
-                memoryUsageAvg2=sample.avg
-        for sample in cclient.statistics.list(meter_name = 'memory.usage', q = query3):
-                memoryUsageAvg3=sample.avg
-	for sample in cclient.statistics.list(meter_name = 'cpu', q = query1):
-		cpuAvg1=sample.avg
+		memoryUsageAvg1 = int(sample.max)
+	for sample in cclient.statistics.list(meter_name = 'memory.usage', q = query2):
+		memoryUsageAvg2 = int(sample.max)
+	for sample in cclient.statistics.list(meter_name = 'memory.usage', q = query3):
+		memoryUsageAvg3 = int(sample.max)
+
+#	for sample in cclient.statistics.list(meter_name = 'cpu', q = query1):
+#		cpuAvg1=sample.avg
+#	for sample in cclient.statistics.list(meter_name = 'cpu', q = query2):
+#		cpuAvg2=sample.avg
+#	for sample in cclient.statistics.list(meter_name = 'cpu', q = query3):
+#		cpuAvg3=sample.avg
+
 	for sample in cclient.statistics.list(meter_name = 'cpu_util', q = query1):
-		cpuUtilsAvg1=sample.avg
-        for sample in cclient.statistics.list(meter_name = 'vcpus', q = query1):
-                vcpusAvg1=sample.avg
-        for sample in cclient.statistics.list(meter_name = 'cpu', q = query2):
-                cpuAvg2=sample.avg
-        for sample in cclient.statistics.list(meter_name = 'cpu_util', q = query2):
-                cpuUtilsAvg2=sample.avg
-        for sample in cclient.statistics.list(meter_name = 'vcpus', q = query2):
-                vcpusAvg2=sample.avg
-        for sample in cclient.statistics.list(meter_name = 'cpu', q = query3):
-                cpuAvg3=sample.avg
-        for sample in cclient.statistics.list(meter_name = 'cpu_util', q = query3):
-                cpuUtilsAvg3=sample.avg
-        for sample in cclient.statistics.list(meter_name = 'vcpus', q = query3):
-                vcpusAvg3=sample.avg
-	for sample in cclient.statistics.list(meter_name = 'disk.allocation', q = query1):
-                diskAloc1=sample.avg
-        for sample in cclient.statistics.list(meter_name = 'disk.allocation', q = query2):
-                diskAloc2=sample.avg
-        for sample in cclient.statistics.list(meter_name = 'disk.allocation', q = query3):
-                diskAloc3=sample.avg
+		cpuUtilsAvg1 = sample.max
+		cpuUtilsAvg1 = float("{0:.2f}".format(cpuUtilsAvg1))
+	for sample in cclient.statistics.list(meter_name = 'cpu_util', q = query2):
+		cpuUtilsAvg2 = sample.max
+		cpuUtilsAvg2 = float("{0:.2f}".format(cpuUtilsAvg2))
+	for sample in cclient.statistics.list(meter_name = 'cpu_util', q = query3):
+		cpuUtilsAvg3 = sample.max
+		cpuUtilsAvg3 = float("{0:.2f}".format(cpuUtilsAvg3))
 
-
+	print "\n\n"
+	print "Duration start:\t" + tsStart
+	print "Duration end:\t" + tsEnd
 	print "\n"
 	print "CPU"
 	print "----"
-        print "**************************************************************************"
-        print "Prosjecno vrijeme iskoristeno - instanca 1:\t\t", cpuAvg1/1000000000, "s"
-        print "Prosjecna iskoristenost  - instanca 1:\t\t\t", cpuUtilsAvg1, "%"
-        print "Prosjecno vrijeme iskoristeno - instanca 2:\t\t", cpuAvg2/1000000000, "s"
-        print "Prosjecna iskoristenost  - instanca 2:\t\t\t", cpuUtilsAvg2, "%"
-        print "Prosjecno vrijeme iskoristeno - instanca 3:\t\t", cpuAvg3/1000000000, "s"
-        print "Prosjecna iskoristenost  - instanca 3:\t\t\t", cpuUtilsAvg3, "%"
+	print "**************************************************************************"
+#	print "Prosjecno vrijeme iskoristeno - instanca 1:\t\t", cpuAvg1/1000000000, "s"
+	print "Maksimalna iskoristenost  - instanca 1:\t\t", cpuUtilsAvg1, "%"
+#	print "Prosjecno vrijeme iskoristeno - instanca 2:\t\t", cpuAvg2/1000000000, "s"
+	print "Maksimalna iskoristenost  - instanca 2:\t\t", cpuUtilsAvg2, "%"
+#	print "Prosjecno vrijeme iskoristeno - instanca 3:\t\t", cpuAvg3/1000000000, "s"
+	print "Maksimalna iskoristenost  - instanca 3:\t\t", cpuUtilsAvg3, "%"
 	print "**************************************************************************"
 
 	print "\n"
 	print "MEMORIJA"
-        print "---------"
-        print "**************************************************************************"
-	print "Alocirano - instanca 1:\t\t\t\t", memoryMax1, "MB"
-        print "Alocirano - instanca 2:\t\t\t\t", memoryMax2, "MB"
-        print "Alocirano - instanca 3:\t\t\t\t", memoryMax3, "MB"
-	print "Prosjecna iskoristenost - instanca 1:\t\t", memoryUsageAvg1, "MB"
-        print "Prosjecna iskoristenost - instanca 2:\t\t", memoryUsageAvg2, "MB"
-        print "Prosjecna iskoristenost - instanca 3:\t\t", memoryUsageAvg3, "MB"
+	print "---------"
+	print "**************************************************************************"
+#	print "Alocirano - instanca 1:\t\t\t\t", memoryMax1, "MB"
+#	print "Alocirano - instanca 2:\t\t\t\t", memoryMax2, "MB"
+#	print "Alocirano - instanca 3:\t\t\t\t", memoryMax3, "MB"
+	print "Maksimalna iskoristenost - instanca 1:\t\t", memoryUsageAvg1, "MB"
+	print "Maksimalna iskoristenost - instanca 2:\t\t", memoryUsageAvg2, "MB"
+	print "Maksimalna iskoristenost - instanca 3:\t\t", memoryUsageAvg3, "MB"
 	print "**************************************************************************"
 
-	print "\n"
-	print "DISK"
-        print "-----"
-        print "**************************************************************************"
-	print "Disk alocation instanca 1:\t\t", diskAloc1/1000000, "MB"
-	print "Disk alocation instanca 2:\t\t", diskAloc2/1000000, "MB"
-	print "Disk alocation instanca 3:\t\t", diskAloc3/1000000, "MB"
-        print "**************************************************************************"
+	stats = tsStart + '\t' + tsEnd + '\t' + str(cpuUtilsAvg1) + '\t' + str(cpuUtilsAvg2) + '\t' + str(cpuUtilsAvg3) + '\t' + str(memoryUsageAvg1) + '\t' + str(memoryUsageAvg2) + '\t' + str(memoryUsageAvg3) + '\n'
+	file.write(stats)
 
-#	cpu_util_sample = cclient.samples.list('cpu_util')
-#	for each in cpu_util_sample:
-#		print each.timestamp, each.resource_id, each.counter_volume
-        #sleep komanda pauzira izvodenje na 3 sekunde nakon cega se ponovno izvode dvije for petlje
-	time.sleep(3)
+file.close()
+
+
--- python/thread.py
+++ python/thread.py
@@ -5,23 +5,31 @@ import time
 import requests
 
 # Define a function for the thread
-def print_time( threadName, delay):
-   count = 0
-   while count < 10:
-      time.sleep(delay)
-      count += 1
-      r=requests.get('http://10.30.2.27/client.php')
-      print threadName, "|| Count: ", count, r
+def print_time( threadName, delay, num):
+	count = 0
+	while count < num:
+		time.sleep(delay)
+		count += 1
+		r=requests.get('http://10.30.2.27/client.php')
+		print threadName, "|| Count: ", count, r
 
-# Create two threads as follows
+
 try:
-   thread.start_new_thread( print_time, ("Thread-1", 1, ) )
-   thread.start_new_thread( print_time, ("Thread-2", 1, ) )
-   thread.start_new_thread( print_time, ("Thread-3", 2, ) )
-   thread.start_new_thread( print_time, ("Thread-4", 1, ) )
-   thread.start_new_thread( print_time, ("Thread-5", 2, ) )
+	thread.start_new_thread( print_time, ("User-X", 10, 360, ) )
+	thread.start_new_thread( print_time, ("User-Y", 20, 180, ) )
+	thread.start_new_thread( print_time, ("User-Z", 30, 120, ) )
+	thread.start_new_thread( print_time, ("User-1", 60, 60, ) )
+	thread.start_new_thread( print_time, ("User-2", 120, 30, ) )
+	thread.start_new_thread( print_time, ("User-3", 180, 20, ) )
+	thread.start_new_thread( print_time, ("User-4", 240, 15, ) )
+	thread.start_new_thread( print_time, ("user-5", 5*60, 12, ) )
+	thread.start_new_thread( print_time, ("User-6", 6*60, 10, ) )
+        thread.start_new_thread( print_time, ("User-7", 10*60, 4, ) )
+        thread.start_new_thread( print_time, ("User-8", 15*60, 2, ) )
+        thread.start_new_thread( print_time, ("User-9", 20*60, 1, ) )
+        thread.start_new_thread( print_time, ("User-10", 20*60, 1, ) )
 except:
-   print "Error: unable to start thread"
+	print "Error: problemi sa dretvama"
 
 while 1:
-  pass
+	pass
