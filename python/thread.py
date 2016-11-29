5fd5fa0c37a77ec92a240bc61d54c8a2e02ebe86
lpejak <lpejak@riteh.hr>
2016-11-29 20:12:17 +0100

    generator sa poissonovim processom

--- python/thread.py
+++ python/thread.py
@@ -3,31 +3,36 @@
 import thread
 import time
 import requests
+import math
+import random
 
 # Define a function for the thread
-def print_time( threadName, delay, num):
+def print_time( threadName):
 	count = 0
-	while count < num:
-		time.sleep(delay)
-		count += 1
-		r=requests.get('http://10.30.2.27/client.php')
-		print threadName, "|| Count: ", count, r
-
+	while count < 100:
+		pp = random.expovariate(1/15.0)
+		time.sleep(math.ceil(pp))
+		if(pp>15.0):
+			count += 1
+			r=requests.get('http://10.30.2.27/client.php')
+			print threadName, " || Count: ", count," || Value: ", pp, r,"\n"
 
 try:
-	thread.start_new_thread( print_time, ("User-X", 10, 360, ) )
-	thread.start_new_thread( print_time, ("User-Y", 20, 180, ) )
-	thread.start_new_thread( print_time, ("User-Z", 30, 120, ) )
-	thread.start_new_thread( print_time, ("User-1", 60, 60, ) )
-	thread.start_new_thread( print_time, ("User-2", 120, 30, ) )
-	thread.start_new_thread( print_time, ("User-3", 180, 20, ) )
-	thread.start_new_thread( print_time, ("User-4", 240, 15, ) )
-	thread.start_new_thread( print_time, ("user-5", 5*60, 12, ) )
-	thread.start_new_thread( print_time, ("User-6", 6*60, 10, ) )
-        thread.start_new_thread( print_time, ("User-7", 10*60, 4, ) )
-        thread.start_new_thread( print_time, ("User-8", 15*60, 2, ) )
-        thread.start_new_thread( print_time, ("User-9", 20*60, 1, ) )
-        thread.start_new_thread( print_time, ("User-10", 20*60, 1, ) )
+	thread.start_new_thread( print_time, ("User-01", ) )
+	thread.start_new_thread( print_time, ("User-02", ) )
+	thread.start_new_thread( print_time, ("User-03", ) )
+	thread.start_new_thread( print_time, ("User-04", ) )
+	thread.start_new_thread( print_time, ("User-05", ) )
+	thread.start_new_thread( print_time, ("User-06", ) )
+	thread.start_new_thread( print_time, ("User-07", ) )
+	thread.start_new_thread( print_time, ("User-08", ) )
+	thread.start_new_thread( print_time, ("User-09", ) )
+	thread.start_new_thread( print_time, ("User-10", ) )
+	thread.start_new_thread( print_time, ("user-11", ) )
+	thread.start_new_thread( print_time, ("User-12", ) )
+        thread.start_new_thread( print_time, ("User-13", ) )
+        thread.start_new_thread( print_time, ("User-14", ) )
+        thread.start_new_thread( print_time, ("User-15", ) )
 except:
 	print "Error: problemi sa dretvama"
 
