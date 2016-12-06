2ba08e63e3e2c4809029d721f3a7b4b77865fb6c
lpejak <lpejak@riteh.hr>
2016-12-06 18:16:17 +0100

    generator ispravljen

--- python/thread.py
+++ python/thread.py
@@ -1,40 +1,21 @@
 #!/usr/bin/python
 
-import thread
 import time
 import requests
 import math
 import random
+from datetime import datetime
 
-# Define a function for the thread
-def print_time( threadName):
+def genTraffic( name):
 	count = 0
-	while count < 100:
-		pp = random.expovariate(1/15.0)
-		time.sleep(math.ceil(pp))
-		if(pp>15.0):
-			count += 1
-			r=requests.get('http://10.30.2.27/client.php')
-			print threadName, " || Count: ", count," || Value: ", pp, r,"\n"
+	while count < 60:
+		pp = random.expovariate(1/0.5)
+		time.sleep(pp)
+		count += 1
+		a= datetime.now()
+		r=requests.get('http://10.30.2.27/client.php')
+		b= datetime.now()
+		c= b-a
+		print name, "-", count," || Value: ",pp, " Start: ",a, " End: "  ,b," Diff: ",c," || ", r,"\n"
 
-try:
-	thread.start_new_thread( print_time, ("User-01", ) )
-	thread.start_new_thread( print_time, ("User-02", ) )
-	thread.start_new_thread( print_time, ("User-03", ) )
-	thread.start_new_thread( print_time, ("User-04", ) )
-	thread.start_new_thread( print_time, ("User-05", ) )
-	thread.start_new_thread( print_time, ("User-06", ) )
-	thread.start_new_thread( print_time, ("User-07", ) )
-	thread.start_new_thread( print_time, ("User-08", ) )
-	thread.start_new_thread( print_time, ("User-09", ) )
-	thread.start_new_thread( print_time, ("User-10", ) )
-	thread.start_new_thread( print_time, ("user-11", ) )
-	thread.start_new_thread( print_time, ("User-12", ) )
-        thread.start_new_thread( print_time, ("User-13", ) )
-        thread.start_new_thread( print_time, ("User-14", ) )
-        thread.start_new_thread( print_time, ("User-15", ) )
-except:
-	print "Error: problemi sa dretvama"
-
-while 1:
-	pass
+genTraffic("User")
